0bf54c7ad6941d55d6fff381f741f57dfb1f2f62
aglegaspi <lex83nyc@gmail.com>
2020-09-04 09:41:26 -0400

    create Item model, load Items in Table View, refactor datasource and delegate code.

---
+++ Todoey/Models/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Alex 6.1 on 9/4/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+}
