12ddbedb9dabe6900824b797264f026340851f75
aglegaspi <lex83nyc@gmail.com>
2020-09-03 21:30:07 -0400

    load dummy data in TableView

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Ang", "Buy Bed", "Take Out Trash"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    //MARK: - TABLEVIEW DATASOURCE METHODS
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
     }
 
 
