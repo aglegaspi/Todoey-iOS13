f7a8e9c5f20d418f0bd47ed5e472fbf5d5436c04
aglegaspi <lex83nyc@gmail.com>
2020-09-05 09:54:25 -0400

    set up TV Datasource methods and Category array

--- Todoey/Controlleres/CategoryViewController.swift
+++ Todoey/Controlleres/CategoryViewController.swift
@@ -7,15 +7,42 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryViewController: UITableViewController {
+    
+    var categoryArray = [Category]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    //MARK: - TABLEVIEW DATASOURCE METHODS
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        let category = categoryArray[indexPath.row]
+        cell.textLabel?.text = category.name
+        return cell
+    }
+    
+    //MARK: - DATA MANIPULATIONS METHODS
+    
+    
+    //MARK: - ADD NEW CATEGORIES
+    
 
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
     }
+    
+
+    //MARK: - TABLEVIEW DELEGATE METHODS
+    
+    
     
 }
