ff559371c965bb6cca50ffbed4fdbb4c65f9a62a
aglegaspi <lex83nyc@gmail.com>
2020-09-04 20:09:13 -0400

    set up CategoryViewController, bar button and action, and update Nav Bar titles.

---
+++ Todoey/Controlleres/CategoryViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Alex 6.1 on 9/4/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+}
