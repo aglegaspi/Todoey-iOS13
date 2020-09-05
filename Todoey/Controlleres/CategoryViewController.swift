c678a1647327a22faa2df68b818f198311f8ea2a
aglegaspi <lex83nyc@gmail.com>
2020-09-05 10:04:14 -0400

    create Category and load in TableView

--- Todoey/Controlleres/CategoryViewController.swift
+++ Todoey/Controlleres/CategoryViewController.swift
@@ -12,9 +12,11 @@ import CoreData
 class CategoryViewController: UITableViewController {
     
     var categoryArray = [Category]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadItems()
     }
     
     //MARK: - TABLEVIEW DATASOURCE METHODS
@@ -24,7 +26,6 @@ class CategoryViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
-        
         let category = categoryArray[indexPath.row]
         cell.textLabel?.text = category.name
         return cell
@@ -34,10 +35,43 @@ class CategoryViewController: UITableViewController {
     
     
     //MARK: - ADD NEW CATEGORIES
-    
-
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
         
+        let alert = UIAlertController(title: "Add New Todey Category", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
+            let newCategory = Category(context: self.context) // stages data to be saved to the persistent container
+            newCategory.name = textField.text!
+            self.categoryArray.append(newCategory)
+            self.saveItems()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Category"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func saveItems() {
+        do {
+            try context.save() // if successful the current state of context to be saved to the persistent container
+        } catch {
+            print("Error saving context with this \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    func loadItems(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        
+        do {
+            self.categoryArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+        tableView.reloadData()
     }
     
 
